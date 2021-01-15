6c9e543de29333cde0abfe1ace0309c3753fe47e
Patryk Sładek <patryksladek@wp.pl>
2021-01-15 22:08:38 +0100

    Add comments

--- Program.cs
+++ Program.cs
@@ -1,40 +1,62 @@
-﻿using System;
+﻿// Użycie przestrzeni nazw System
+using System;
 
-namespace NumberGuesser
+// Przestrzeń nazw
+namespace NumberGuesser 
 {
-    class Program
+    // Klasa główna
+    class Program 
     {
-        static void Main(string[] args)
+        // Metoda główna, czyli punkt wejścia programu 
+        static void Main(string[] args) 
         {
+            // Wyświetl informacje o aplikacji
             GetAppInfo();
 
+            // Zapytaj o imię i zainicjalizuj zmienną typu tesktowego
             string userName = GetUserName();
 
+            // Przywitaj użytkownika
             GreetUser(userName);
 
+            // Wylosuj liczbę z przedziału od 1 do 10
             Random random = new Random();
-            int correctNumber =  random.Next(1, 11); 
+            // Zainicjalizuj zmienną typu całkowtego
+            int correctNumber =  random.Next(1, 11);
 
+            // Zainicjalizuj zmienną typu logicznego
             bool correctAnswer = false;
 
+            // Wyświetl komunikat
             Console.WriteLine("Zgadnij wylosowaną liczbę z przedziału od 1 do 10.");
 
+            // Powtarzaj operacje w pętli dopóki wartość zmiennej correctAnswer jest równa false
             while (!correctAnswer)
             {
+                // Pobierz i zainicjalizuj zmienną typu tesktowego
                 string input = Console.ReadLine();
 
+                // Zadeklaruj zmienną typu liczbowego
                 int guess;
 
+                // Konwertuj typ tesktowy na typ liczbowy
                 bool isNumber = int.TryParse(input, out guess);
+                
+                // Sprawdź czy użytkownik wprowadził liczbę
                 if (!isNumber)
                 {
+                    // Wyświetl komunikat 
                     PrintColorMessage(ConsoleColor.Yellow, "Proszę wprowadzić liczbę.");
+                    // Przejdź do kolejnej iteracji pętli
                     continue;
                 }
 
+                // Sprawdź czy użytkownik wprowadził liczbę z przediału od 1 do 10
                 if (guess < 1 || guess > 10)
                 {
+                    // Wyświetl komunikat
                     PrintColorMessage(ConsoleColor.Yellow, "Proszę wprowadzić liczbę z przedziału od 1 do 10.");
+                    // Przejdź do kolejnej iteracji pętli
                     continue;
                 }
 
@@ -54,42 +76,58 @@ namespace NumberGuesser
             }
         }
 
+        // Wyświetl informacje o aplikacji
         static void GetAppInfo()
         {
+            // Zainicjalizuj informacje o aplikacji
             string appName = "Zgadywanie liczby";
             int appVersion = 1;
             string appAuthor = "Patryk Sładek";
 
+            // Przygotuj tekst informacji
             string info = $"[{appName}] Wersja: 0.0.{appVersion}, Autor: {appAuthor}";
 
+            // Wyświetl komunikat w fioletowym kolorze
             PrintColorMessage(ConsoleColor.Magenta, info);
 
         }
 
+        // Zapytaj o imię i zwróć wartość typu tesktowego
         static string GetUserName()
         {
+            // Zapytaj o imię
             Console.WriteLine("Jak masz na imię?");
 
+            // Pobierz odpowiedź użytkownika
             string inputUserName = Console.ReadLine();
 
+            // Zwróć odpowiedź
             return inputUserName;
         }
 
-        static void GreetUser(string userName)
+        // Przywitaj użytkownika
+        static void GreetUser(string userName) // Parametrem wejściowym metody jest imię użytkownika 
         {
+            // Przygotuj tekst przywitania
             string greet = $"Powodzenia {userName}, odgadnij liczbę...";
 
+            // Wyświetl komunikat w niebieskim kolorze
             PrintColorMessage(ConsoleColor.Blue, greet);
 
+            // Wyświetl pustą linię na konsoli
             Console.WriteLine();
         }
 
+        // Wyświetl komunikat w odpowiednim kolorze
         static void PrintColorMessage(ConsoleColor color, string message)
         {
+            // Zmień kolor tekstu konsoli
             Console.ForegroundColor = color;
 
+            // Wyświetl komunikat na konsoli
             Console.WriteLine(message);
 
+            // Zresetuj kolor tekstu konsoli
             Console.ResetColor();
         }
     }
